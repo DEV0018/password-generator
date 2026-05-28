5d7b3f7bf8ab257c91596b1fa245d31330692de2
DEV0018 <cdevaraj0018@gmail.com>
2026-05-28 23:17:02 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function generatePassword() {
+
+  let length = document.getElementById("length").value;
+
+  let chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+
+    let randomIndex =
+      Math.floor(Math.random() * chars.length);
+
+    password += chars[randomIndex];
+  }
+
+  document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+
+  let passwordField =
+    document.getElementById("password");
+
+  passwordField.select();
+
+  navigator.clipboard.writeText(passwordField.value);
+
+  alert("Password Copied!");
+}
